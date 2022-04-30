9091bcc226c074dd8c9b11118b3928c2c2519716
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-04-30 00:43:43 -0300

    a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        Artist artist = new Artist();
+        artist.CPF = 02265377651; 
+
+        Console.WriteLine(artist.CPF);
+
+    }
+}
