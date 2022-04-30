df2920d0023c8f3b07b44370ea1f4041a28fba99
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-04-30 09:33:32 -0300

    add

--- Program.cs
+++ Program.cs
@@ -2,14 +2,10 @@
 public class Program{
     static void Main(string[] args){
 
-        Artist artist = new Artist("Labrinth", "022.653.776-51", new DateTime(1989, 01, 04), new DateTime(), "Eletrónica", new DateTime(2021, 08, 12));
+        Artist Labrinth = new Artist("Labrinth", "022.653.776-51", new DateTime(1989, 01, 04), new DateTime(), "Eletrónica", new DateTime(2021, 08, 12));
 
-        Console.WriteLine(artist.Name);
-        Console.WriteLine(artist.CPF);
-        Console.WriteLine(artist.BirthDate);
-        Console.WriteLine(artist.DeathDate);
-        Console.WriteLine(artist.Musical_genre);
-        Console.WriteLine(artist.ContractDate);
+        Producer Masterworks = new Producer("Milan Records", "061.743.987-12", new DateTime(1983, 03, 19), new DateTime(), "Masterworks");
 
+        Album album = new Album(Labrinth, Masterworks, "Euphoria", "Eletrónica", "Euphoria (Original Score from the HBO Series)", new DateTime(2019, 10, 04));
     }
 }
