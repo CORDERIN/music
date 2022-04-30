b955ac7637add44e2ca81644fcde18f1aeb6abc0
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-04-30 00:39:59 -0300

    a

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program{
+
+    static void Main(){
+
+        Artist artist = new Artist();
+        artist.CPF = 02265377651; 
+        Console.WriteLine(artist.CPF);
+
+    }
+}
