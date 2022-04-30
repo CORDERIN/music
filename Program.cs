3a2c699b9364b66fbc596e5ed828aea39bc776ed
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-04-30 11:23:48 -0300

    melhorias

--- Program.cs
+++ Program.cs
@@ -2,26 +2,26 @@
 public class Program{
     static void Main(string[] args){
 
-        //Artista Labrinth
+        //Artistas
 
         Artist Labrinth = new Artist("Labrinth", "022.653.776-51", new DateTime(1989, 01, 04), new DateTime(), "Eletrónica", new DateTime(2021, 08, 12));
 
-        //Produtora Masterwords
+        //Produtoras
 
         Producer Masterworks = new Producer("Milan Henry", "061.743.987-12", new DateTime(1983, 03, 19), new DateTime(), "Masterworks");
 
-        //Gêneros musicais que a produrora Masterworks produz
+        //Gêneros musicais que a produrora produz
 
         Masterworks.Musical_genres.Add("Eletrônica");
         Masterworks.Musical_genres.Add("Rap");
         Masterworks.Musical_genres.Add("Pop");
         Masterworks.Musical_genres.Add("Rock");
 
-        //Albúm Euphoria
+        //Álbum Euphoria
 
         Album Euphoria = new Album(Labrinth, Masterworks, "Euphoria", "Eletrónica", "Euphoria (Original Score from the HBO Series)", new DateTime(2019, 10, 04));
 
-        //Músicas soltas (todas de Euphoria)
+        //Inicializando músicas "soltas"
 
         Music Mount_Everest = new Music("Mount Everest", 2.6 , "Eletrónica", new DateTime(2019, 10, 04));
         Music Formula = new Music("Formula", 3.5, "Eletrónica", new DateTime(2019, 10, 04));
@@ -36,7 +36,7 @@ public class Program{
         Music ArrivalAtTheFormal = new Music("Arrival At The Formal", 5.95, "Eletrónica", new DateTime(2019, 10, 04));
         Music Gangster = new Music("Gangster", 2.5, "Eletrónica", new DateTime(2019, 10, 04));
 
-        //Adicionando as músicas ao álbum de Euphoria
+        //Adicionando as músicas ao álbum Euphoria
 
         Euphoria.Musics.Add(Mount_Everest);
         Euphoria.Musics.Add(Formula);
@@ -50,6 +50,14 @@ public class Program{
         Euphoria.Musics.Add(WTF_AreWeTalkingFor);
         Euphoria.Musics.Add(ArrivalAtTheFormal);
         Euphoria.Musics.Add(Gangster);
+
+        //Adicionando os álbumns do cantor
+
+        Labrinth.ArtistAlbums.Add(Euphoria);
+
+        //Adicionando os álbumns da produtora
+
+        Masterworks.ProducerAlbums.Add(Euphoria);
 
         //Percorrendo e imprimindo as músicas do Álbum Euphoria
 
