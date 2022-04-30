fc277ea67ffaf4bd397a813d92a9d7485282660a
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-04-30 00:42:49 -0300

    melhorias

--- Program.cs
+++ Program.cs
@@ -1,16 +1,2 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public class Program{
-
-    static void Main(){
-
-        Artist artist = new Artist();
-        artist.CPF = 02265377651; 
-        Console.WriteLine(artist.CPF);
-
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
